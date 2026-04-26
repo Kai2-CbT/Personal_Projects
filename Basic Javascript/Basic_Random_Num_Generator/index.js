9304dd2f40a4e6ae864c2e18dd9647b1fdb42513
ChinBo Tu <ctu11@my.centennialcollege.ca>
2026-04-26 04:18:23 -0400

    added totals to randomNumber generator app

--- Basic Javascript/Basic_Random_Num_Generator/index.js
+++ Basic Javascript/Basic_Random_Num_Generator/index.js
@@ -16,17 +16,48 @@ document.getElementById("dice_3_display").textContent = dice_3;
 
 //add functionality to each button
 
-document.getElementById("roll_Dice1").onclick = function(){
+document.getElementById("rollDice_1").onclick = function() {
     dice_1 = Math.floor(Math.random() * max) + min;
     document.getElementById("dice_1_display").textContent = dice_1;
+    document.getElementById("dice_2_display").textContent = "";
+    document.getElementById("dice_3_display").textContent = "";
 
+let total = document.getElementById("total_counter");
+    let totalValue;
+
+    totalValue = dice_1;
+
+    total.textContent = totalValue;
 };
 
 document.getElementById("roll2Dice").onclick = function(){
+    dice_1 = Math.floor(Math.random() * max) + min;
+    dice_2 = Math.floor(Math.random() * max) + min;
+    document.getElementById("dice_1_display").textContent = dice_1;
+    document.getElementById("dice_2_display").textContent = dice_2;
+    document.getElementById("dice_3_display").textContent = "";
 
+    let total = document.getElementById("total_counter");
+    let totalValue;
+
+    totalValue = dice_1 + dice_2;
+
+    total.textContent = totalValue;
 };
 
 document.getElementById("roll3Dice").onclick = function(){
+    dice_1 = Math.floor(Math.random() * max) + min;
+    dice_2 = Math.floor(Math.random() * max) + min;
+    dice_3 = Math.floor(Math.random() * max) + min;
+    document.getElementById("dice_1_display").textContent = dice_1;
+    document.getElementById("dice_2_display").textContent = dice_2;
+    document.getElementById("dice_3_display").textContent = dice_3;
+    let total = document.getElementById("total_counter");
+    let totalValue;
 
+    totalValue = dice_1 + dice_2 + dice_3;
+
+    total.textContent = totalValue;
 };
+
 
