ac37039387c0ece5a547eae7dda8906bea1498f3
ChinBo Tu <ctu11@my.centennialcollege.ca>
2026-04-28 05:06:44 -0400

    updated game logic

--- Basic Javascript/Number Guesser Game/index.js
+++ Basic Javascript/Number Guesser Game/index.js
@@ -13,13 +13,41 @@ let start = document.getElementById("startBtn").onclick = function(){
     const answer = Math.floor(Math.random() * max + min);
     let running = true;
     let guess;
+    let attempts = 0;
     //then loop for guesses
     while(running){
         guess = window.prompt("guess number between 1 - 100")
-        guess = Number(guess)
-        alert(guess);
+        
+        if(guess === null){
+                window.alert("Game cancelled");
+                running = false;
+                break;
+            }
+
+            guess = Number(guess)
+        if(isNaN(guess)){
+            window.alert("enter an actual number")
+        }
+        else{
+
+        
+        attempts++;
+        //conditional checks
+        if(guess < answer){
+            window.alert("Too low!");
+        }
+        else if (guess > answer){
+            window.alert("Too High!");
+        }
+        else{
+            window.alert("correct!");
+            running = false;
+        }
     };
 
+
+    };
+    document.getElementById("attempts_counter").textContent = attempts;
 
     
 }
