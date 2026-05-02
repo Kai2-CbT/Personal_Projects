a8e5e3081fe83169873f822ac27623b18b83e204
ChinBo Tu <ctu11@my.centennialcollege.ca>
2026-05-02 08:33:17 -0400

    password generator inital update

--- Basic Javascript/Dice Roller/index.js
+++ Basic Javascript/Dice Roller/index.js
@@ -1,5 +1,28 @@
 //program needs to allow user to input a number of dice to roll
-
+let diceResult = document.getElementById("diceDisplay");
+let diceImage = document.getElementById("diceImage");
 //then a function that is called to roll dice X number of input
+function diceRoll(){
+    const diceNum = document.getElementById("diceValue").value;
+    Number(diceNum);
+
+    //array to store dice roll values
+    let values = [];
+    //array for dice images
+    const images = [];
+
+    //for loop to roll dice for number of dice
+    for(i = 0; i < diceNum; i++ ){
+        //generate a random number between 1-6 and store it in the array.
+        const value = Math.floor(Math.random() * 6 + 1);
+        //push the rolled value into the array values
+        values.push(value);
+        //push 
+        images.push(`<img src="images/${value}.png">`);
+
+    }
+    diceResult.textContent = `dice: ${values.join(' ')}`;
+    diceImage.innerHTML = images.join(` `);
+}
 
 //store each dice result as part of an array, then just display each element in the array.
---
+++ Basic Javascript/Random Password Generator/index.js
@@ -0,0 +1,31 @@
+//variables to hold types of password characters
+    const passwordLength = 8;
+    const lowercase = true;
+    const uppercase = true;
+    const numbers = true;
+    const symbols = true;
+    //variable to hold new password
+
+
+
+//declare funtion to generate password
+
+
+function generatePassword(passwordLength, lowercase, uppercase, numbers, symbols){
+    //holds final pass
+        const password = " ";
+        //holds characters for password type
+        const allowedChars = " ";
+
+        //varaiblestrings for each type
+        let lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+        let upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let nums = "0123456789";
+        let syms = "!@#$%^&*()";
+
+        
+
+    
+
+    //loop
+}
